d3fe9787215f3a3c12bfe3f22b97e55e166b1656
Berni Ruoff <bernesterland@gmail.com>
2023-03-26 13:05:27 +0200

    added a datenschutzerklaeriung

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,75 @@
+<script>
+  import "../global.scss";
+</script>
+
+<svelte:head>
+  <title>Zeltlager Baltersberg - Freizeit für Alle!</title>
+</svelte:head>
+<div class="layout">
+  <header class="header">
+    <a href="/" aria-label="Zur Startseite">
+      <img src="baltersberg-logo.svg" alt="" class="logo" aria-hidden="true" />
+    </a>
+  </header>
+  <main class="main">
+    <slot />
+  </main>
+  <footer class="footer">
+    <div class="wrapper">
+      <p>
+        Das Zeltlager Baltersberg ist unter der Trägerschaft des
+        <a
+          href="https://allgaeu-oberschwaben.bdkj.info/"
+          target="_blank"
+          rel="nofollow"
+        >
+          Katholischen Jugendreferat Ravensburg
+        </a>. <br />
+        <a href="/datenschutz">Datenschutzerklärung</a>
+        &copy; 2017-2023 Zeltlager Baltersberg
+      </p>
+    </div>
+  </footer>
+</div>
+
+<style>
+  .logo {
+    height: 64px;
+  }
+
+  .layout {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .header {
+    display: flex;
+    justify-content: flex-end;
+    padding: 32px;
+    flex: 0;
+  }
+
+  .main {
+    padding: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+  }
+
+  .footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 0;
+    font-size: 12px;
+  }
+
+  .wrapper {
+    box-sizing: border-box;
+    max-width: 640px;
+    text-align: center;
+    padding-bottom: 80px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,83 +1,10 @@
-<svelte:head>
-  <title>Zeltlager Baltersberg - Freizeit für Alle!</title>
-</svelte:head>
-<div class="layout">
-  <header class="header">
-    <img src="baltersberg-logo.svg" alt="Zeltlager Baltersberg" class="logo" />
-  </header>
-  <main class="main">
-    <div class="wrapper">
-      <h1 class="h1 bakso text-center">
-        <span class="h3">Zeltlager Baltersberg</span><br /> Freizeit für Alle!
-      </h1>
-      <p class="lead text-center">
-        Wir sind gerade am Umbauen. Bei Fragen schreiben Sie bitte an <a
-          href="mailto: freizeitfueralle@gmail.com"
-          >freizeitfueralle@gmail.com</a
-        >
-      </p>
-    </div>
-  </main>
-  <footer class="footer">
-    <div class="wrapper">
-      <p>
-        Das Zeltlager Baltersberg ist unter der Trägerschaft des
-        <a
-          href="https://allgaeu-oberschwaben.bdkj.info/"
-          target="_blank"
-          rel="nofollow"
-        >
-          Katholischen Jugendreferat Ravensburg
-        </a>. <br />
-        &copy; 2017-2023 Zeltlager Baltersberg
-      </p>
-    </div>
-  </footer>
+<div>
+  <h1 class="h1 bakso text-center">
+    <span class="h3">Zeltlager Baltersberg</span><br /> Freizeit für Alle!
+  </h1>
+  <p class="lead text-center">
+    Wir sind gerade am Umbauen. Bei Fragen schreiben Sie bitte an <a
+      href="mailto: freizeitfueralle@gmail.com">freizeitfueralle@gmail.com</a
+    >
+  </p>
 </div>
-
-<style>
-  .logo {
-    height: 64px;
-  }
-
-  .layout {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .header {
-    display: flex;
-    justify-content: flex-end;
-    padding: 32px;
-    flex: 0;
-  }
-
-  .main {
-    padding: 32px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex: 1;
-  }
-
-  .footer {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex: 0;
-    font-size: 12px;
-  }
-
-  .wrapper {
-    box-sizing: border-box;
-    max-width: 640px;
-    text-align: center;
-    padding-bottom: 80px;
-  }
-
-  .lead {
-    font-size: 1.5rem;
-    line-height: 1.5em;
-  }
-</style>
