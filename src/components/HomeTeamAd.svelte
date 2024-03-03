d28ce15ab2d1b42a6ec2c52282ddd327723aa0d5
Berni Ruoff <berni.ruoff@thoughtworks.com>
2024-03-03 17:07:10 +0100

    changed color for home teaser

--- src/components/HomeTeamAd.svelte
+++ src/components/HomeTeamAd.svelte
@@ -3,13 +3,14 @@
     <div class="bento">
       <h2 class="bakso h2">Mach mit!</h2>
       <p class="copy">Werde Teil eines einzigartigen Sommerabenteuers.</p>
-      <a href="/mach-mit" class="btn btn-small">Mehr erfahren</a>
+      <a href="/mach-mit" class="btn btn-secondary btn-small">Mehr erfahren</a>
     </div>
   </div>
 </section>
 
 <style lang="scss">
   .bento {
-    background-color: #e0f2f1;
+    background-color: var(--text);
+    color: var(--logo-sign);
   }
 </style>
