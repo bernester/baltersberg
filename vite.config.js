f7ec21e49f422b7c369c8519d512baaf156fe5dd
Bernhard Ruoff <bernesterland@gmail.com>
2025-01-05 11:16:43 +0100

    added aliases

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import path from 'path';
 
 export default defineConfig({
   plugins: [sveltekit()],
   resolve: {
     alias: {
-      '@utils': '/src/lib/utils',
-      '@components': '/src/components/',
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@components': path.resolve(__dirname, 'src/components'),
     }
   }
 });
