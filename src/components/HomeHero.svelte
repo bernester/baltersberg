5abb6175c9369d037b3b7d941b7ba73e0991cae4
Bernhard Ruoff <bernesterland@gmail.com>
2025-02-08 08:47:34 +0100

    updated hero

--- src/components/HomeHero.svelte
+++ src/components/HomeHero.svelte
@@ -1,4 +1,5 @@
 <script type="module">
+  import { formatDate } from "$lib/utils";
   import data from "./../data.json";
   import Headline from "./Headline.svelte";
 </script>
@@ -10,11 +11,17 @@
     <p class="text-2xl/relaxed font-bold">
       Das inklusive Sommerabenteuer für Alle.
     </p>
-
+    <div class="text-4xl/relaxed font-bolle">
+      <mark
+        >{formatDate(data.dates.CurrentStart)} - {formatDate(
+          data.dates.CurrentEnd
+        )}</mark
+      >
+    </div>
 
     <a
       href="https://tally.so/r/3EKKMN"
-       class="inline-block px-6 py-3.5 text-base font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      class="inline-block px-6 py-3.5 text-base font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       rel="nofollow"
       target="blank">Jetzt anmelden</a
     >
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,22 @@
 <script>
-  import HomePrices from "../components/HomePrices.svelte";
-  import HomeHero from "../components/HomeHero.svelte";
-  import HomeSummary from "../components/HomeSummary.svelte";
-  import HomeTeamAd from "../components/HomeTeamAd.svelte";
-  import data from "./../data.json";
-  import { formatDate } from "$lib/utils";
-  import Headline from "@components/Headline.svelte";
+  import HomePrices from "$components/HomePrices.svelte";
+  import HomeHero from "$components/HomeHero.svelte";
+  import HomeSummary from "$components/HomeSummary.svelte";
+  import HomeTeamAd from "$components/HomeTeamAd.svelte";
+  import Headline from "$components/Headline.svelte";
 </script>
 
 <HomeHero />
-
-<div class="text-4xl/relaxed font-bolle">
-  Das Abenteuer geht weiter:<br />
-  <mark
-    >{formatDate(data.dates.CurrentStart)} - {formatDate(
-      data.dates.CurrentEnd
-    )}</mark
-  ><br />
-  Anmeldung ab Februar 2025
-</div>
 
 <HomePrices />
 
 <HomeSummary />
 
-<section class="max-w-screen-sm">
+<article class="max-w-screen-sm">
   <div>
-    <Headline>Was ist das Zeltlager Baltersberg?</Headline>
+    <Headline>Was ist die Freizeit für Alle?</Headline>
     <p class="copy">
-      Das Zeltlager Baltersberg ist ein inklusives Zeltlager für Kinder und
+      Die Freizeit für Alle ist ein inklusives Zeltlager für Kinder und
       Jugendliche zwischen 10 und 15 Jahren! Bei uns sind alle herzlich
       willkommen, unabhängig von Fähigkeiten, Geschlecht, Religion oder
       Hintergrund. Wir bieten ein abenteuerliches Programm voller Spaß und
@@ -37,6 +25,6 @@
       Erlebnissen!
     </p>
   </div>
-</section>
+</article>
 
 <HomeTeamAd />
