bb23ac60069e4e77813be1688482b5e93be382a0
Bernhard Ruoff <bernesterland@gmail.com>
2025-02-08 08:47:27 +0100

    use alias for components and lib

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-vercel';
+import path from 'path';
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
+    alias: {
+      $components: path.resolve('src/components'),
+      $lib: path.resolve('src/lib')
+    }
   },
   preprocess: vitePreprocess(),
 };
