7fe524cd733e78bf1eed42f58e0b599bc1620115
Bernhard Ruoff <bernesterland@gmail.com>
2025-02-09 09:50:46 +0100

    added imprint page

---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,44 @@
+<div class="max-w-screen-sm mx-auto py-8 mb-8">
+  <section class="mb-4">
+    <h1 class="text-lg font-bold mb-2">Impressum</h1>
+    <p class="mb-4 text-sm"><strong>Angaben gemäß § 5 TMG:</strong></p>
+    <p class="mb-4 text-sm">
+      Freieitbande e.V<br />
+      c/o Bernhard Ruoff<br />
+      Bronngasse 4<br />
+      71083 Herrenberg
+    </p>
+  </section>
+  <section class="mb-4">
+    <h2 class="text-md font-bold mb-2">Kontakt</h2>
+    <p class="mb-4 text-sm">E-Mail: freizeitfueralle@gmail.com</p>
+    <p class="mb-4 text-sm">
+      <strong>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</strong>
+    </p>
+    <p class="mb-4 text-sm">
+      Freieitbande e.V<br />
+      c/o Bernhard Ruoff<br />
+      Bronngasse 4<br />
+      71083 Herrenberg
+    </p>
+  </section>
+  <section class="mb-4">
+    <h2 class="text-md font-bold mb-2">Haftungsausschluss:</h2>
+    <p class="mb-4 text-sm">
+      <strong>Haftung für Inhalte</strong><br />
+      Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit,
+      Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen.
+    </p>
+    <p class="mb-4 text-sm">
+      <strong>Haftung für Links</strong><br />
+      Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte
+      wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch
+      keine Gewähr übernehmen.
+    </p>
+    <p class="mb-4 text-sm">
+      <strong>Urheberrecht</strong><br />
+      Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten
+      unterliegen dem deutschen Urheberrecht. Beiträge Dritter sind als solche gekennzeichnet.
+    </p>
+  </section>
+</div>
