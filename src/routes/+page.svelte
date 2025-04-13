0b668f92f56deb6f80acea2dd03c01f6f868f1c5
Bernhard Ruoff <bernesterland@gmail.com>
2025-04-13 17:14:34 +0200

    added video link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,34 @@
 
 <div class="space-y-20">
   <HomeHero />
+  <section class="p-8">
+    <a
+      href="https://www.youtube.com/watch?v=fllxNphPCqw"
+      target="_blank"
+      class="relative block group"
+    >
+      <div
+        class="absolute inset-0 flex items-center justify-center z-10"
+        role="presentation"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-20 h-20 text-black bg-white group-hover:bg-opacity-90 transition duration-200 rounded-full p-4"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <path d="M8 5v14l11-7z" />
+        </svg>
+      </div>
+
+      <img
+        src="videothumbnail-gross.jpg"
+        class="w-full max-w-4xl mx-auto transform transition-transform duration-200 group-hover:scale-105"
+        alt="Die Freizeitbande sitzt um das Lagerfeuer herum und singt"
+      />
+      <span class="sr-only">Youtube Video von der Freizeitbande</span>
+    </a>
+  </section>
   <HomePrices />
   <HomeSummary />
   <HomeSeo />
