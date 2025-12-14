b949060f0478d323e8a2433e4c73f3f3d8bba00b
Bernhard Ruoff <bernesterland@gmail.com>
2025-12-14 15:20:43 +0100

    fix: update color transparency values in tailwind configuration
    - Increased transparency for yellow and green colors from 0.6 to 0.8 in tailwind.config.js for improved visual clarity.

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,12 +16,12 @@ export default {
         white: "#f5f5f5",
         yellow: {
           DEFAULT: "oklch(100% 0.22 112)",
-          transparent: "oklch(100% 0.22 112 / .6)",
+          transparent: "oklch(100% 0.22 112 / .8)",
           light: "oklch(100% 0.14 112)",
         },
         green: {
           DEFAULT: "oklch(85% 0.26 151)",
-          transparent: "oklch(85% 0.26 151 / .6)",
+          transparent: "oklch(85% 0.26 151 / .8)",
           light: "oklch(85% 0.20 151)",
         },
       },
